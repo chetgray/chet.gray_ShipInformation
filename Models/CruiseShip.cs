586e5ae023dc94d74bd224b25f73695046d3710d
Chet Gray <chet.gray@waystar.com>
2022-09-27 16:49:30 -0400

    ✨ feat: `CruiseShip` derived class
    > - You should have a class called CruiseShip that derives from Ship.
    >   - When you blow the horn on the Cruise Ship, it should write
    >     "WAAAAAAAAOOOOOOO!" the Console.
    >   - When the Cruise Ship moves, it should first write "Starting all 112
    >     engines" to the Console, then it should write the standard verbiage
    >     from the base class.
    >   - When the Cruise Ship stops, it should first write, "Stopping all
    >     112 engines" to the Console, then it should write the standard
    >     verbiage from the base class.

---
+++ Models/CruiseShip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ShipInformation.Models
+{
+    internal class CruiseShip : Ship
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Starting all 112 engines");
+            base.Move();
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Stopping all 112 engines");
+            base.Stop();
+        }
+
+        public override void BlowHorn()
+        {
+            System.Console.WriteLine("WAAAAAAAAOOOOOOO!");
+        }
+    }
+}
