99a72938d4e6bad9f594a8fdbfde464dbf78f342
Chet Gray <chet.gray@waystar.com>
2022-09-27 16:42:44 -0400

    ✨ feat: `Ship` abstract base class
    > - You'll want to have an abstract Ship base class.
    >   - It should have a method called Move().
    >     - This method should write to the Console that "The ship has started
    >       to move."
    >   - It should have a method called Stop().
    >     - This method should write to the Console that "The ship has stopped."
    >   - It should have a BlowHorn() method. This will be completely abstract,
    >     as the implementation will be provided by each derived class.

---
+++ Models/Ship.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ShipInformation.Models
+{
+    internal abstract class Ship
+    {
+        public virtual void Move()
+        {
+            Console.WriteLine("The ship has started to move.");
+        }
+
+        public virtual void Stop()
+        {
+            Console.WriteLine("The ship has stopped.");
+        }
+
+        public abstract void BlowHorn();
+    }
+}
