6711900d9be086a0423135f998293c31587d5f92
Chet Gray <chet.gray@waystar.com>
2022-09-28 10:22:33 -0400

    ✨ feat: prompt for ship type
    > - When the application starts, ask the user what type of ship they are
    >   running. Their choices should be numbered 1, 2, and 3, for each ship
    >   type.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,41 @@
-﻿namespace ShipInformation
+﻿using System;
+using ShipInformation.Models;
+
+namespace ShipInformation
 {
     internal static class Program
     {
         static void Main()
         {
+            Ship ship = null;
+
+            do
+            {
+                Console.Write(
+                    "What type of ship are you running?\n"
+                        + "- [1] Cruise Ship - [2] Sail Boat - [3] Ski Boat -\n» "
+                );
+                string shipInput = Console.ReadLine().ToLower().Replace(" ", null);
+                switch (shipInput)
+                {
+                    case "1":
+                    case "cruiseship":
+                    case "cruise":
+                        ship = new CruiseShip();
+                        break;
+                    case "2":
+                    case "sailboat":
+                    case "sail":
+                    case "3":
+                    case "skiboat":
+                    case "ski":
+                    default:
+                        Console.WriteLine("ERROR: Invalid ship type selection.");
+                        break;
+                }
+            } while (ship is null);
+
+            Console.ReadKey(intercept: true);
         }
     }
 }
